c6abc541b1e98ca4cbba26069eb83b6ae085b257
davidmellis <david.ellis@iongroup.com>
2023-11-01 17:06:03 +0000

    mods to gitme

--- src/changeversion/gitme.py
+++ src/changeversion/gitme.py
@@ -1,14 +1,40 @@
-from git import Repo
+from git import Repo, Actor
+from changeversion.versh import VersionHolder
 
 class DoGit:
     def play(self):
 
         repo = Repo(".")
 
-        repo.create_tag("past",
-                        message="This is a tag-object pointing to %s" % "new_branch",
-)
+        file=open("VERSION","r")
+        version_number = file.read()
 
+        current_version = VersionHolder(version_number)
+        print(current_version.rep())
+
+        new_version = current_version.bump('micro')
+        f = open("VERSION", "w")
+        f.write(new_version.rep())
+        f.close()
+
+        version = new_version
+
+#        repo.create_tag("v" + version,
+#                        message="This is a tag-object pointing to %s" % "new_branch",
+#        )
+        index = repo.index
+        author = Actor("An author", "author@example.com")
+        committer = Actor("A committer", "committer@example.com")
+        # commit by commit message and author and committer
+        index.add('VERSION')
+        index.commit("changeversion to v" + version.rep())
+
+        print("COMMITTED NEW VERSION " + version.rep())
+
+        origin = repo.remotes.origin
+        print("PUSHING ...")
+        origin.push()
+        print("DONE PUSHING")
 
 DoGit().play()
 
