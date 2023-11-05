47d46ee69a3e1fa6f7861bdf5a83368a4c357868
davidmellis <david.ellis@iongroup.com>
2023-11-05 12:41:25 +0000

    david: go to http not https

--- list_versions.py
+++ list_versions.py
@@ -3,7 +3,7 @@ from requests.auth import HTTPBasicAuth
 from distutils.version import LooseVersion
 
 def versions(package_name, limit_releases=10):
-    url = "https://18.170.223.248:8080/changeversion/json"
+    url = "http://18.170.223.248:8080/changeversion/json"
     data = requests.get(url, verify=False).json()
     versions = list(data["releases"].keys())
     versions.sort(key=LooseVersion, reverse=True)
