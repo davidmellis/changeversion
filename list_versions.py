6fd7a62ad01346e2a08079bf8bc2e09243bc19b9
davidmellis <david.ellis@iongroup.com>
2023-11-05 12:36:47 +0000

    david: oops using pip by mistake

--- list_versions.py
+++ list_versions.py
@@ -4,10 +4,6 @@ from distutils.version import LooseVersion
 
 def versions(package_name, limit_releases=10):
     url = "https://18.170.223.248:8080/changeversion/json"
-    s = requests.Session()
-    #s.headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36'
-
-    # s.max_redirects = 560
     data = requests.get(url, verify=False).json()
     versions = list(data["releases"].keys())
     versions.sort(key=LooseVersion, reverse=True)
