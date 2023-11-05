47799f7c1c600fa2a7c6c35dbdcb90c1bdfdcc7b
davidmellis <david.ellis@iongroup.com>
2023-11-05 12:08:48 +0000

    david: allow trusted host

--- list_versions.py
+++ list_versions.py
@@ -3,7 +3,7 @@ from requests.auth import HTTPBasicAuth
 from distutils.version import LooseVersion
 
 def versions(package_name, limit_releases=10):
-    url = "http://18.170.223.248:8080/changeversion/json"
+    url = "https://18.170.223.248:8080/changeversion/json"
     s = requests.Session()
     #s.headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36'
 
