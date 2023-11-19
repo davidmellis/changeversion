1df10e8d5b8ef5f109531f167014dd6f38b52607
David Ellis <david.ellis@iongroup.com>
2023-11-19 12:05:02 +0000

    david: files to test bump

--- list_versions.py
+++ list_versions.py
@@ -3,7 +3,7 @@ import requests
 from requests.auth import HTTPBasicAuth
 from distutils.version import LooseVersion
 
-def versions(package_name, limit_releases=10):
+def versions(package_name, limit_releases=500):
     url = "http://18.170.223.248:8080/" + sys.argv[1] + "/json"
     print("URL: " + url)
     data = requests.get(url, verify=False).json()
