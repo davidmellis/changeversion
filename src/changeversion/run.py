4ae0b8f2326c2c09a70be1b1897e4fb63edec875
David Ellis <david.ellis@iongroup.com>
2023-11-20 09:24:13 +0000

    david: tidyup for bump2

--- src/changeversion/run.py
+++ src/changeversion/run.py
@@ -1,10 +1,1 @@
-from changeversion.progress import ProgressIndicator
-from changeversion.gitme import DoGit
-from changeversion.versh import VersionHolder, Vershion
-
-# gitme = DoGit()
-
-# v = VersionHolder("9.0.17")
-
-# gitme.tag_version(v)
 
